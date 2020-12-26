e4e956b0c26d56bf15ae76de83bbe4b0d70787a6
chandi <git@chandi.it>
2020-12-26 18:53:01 +0100

    bbb-playback: 1:2.3.0-3

---
+++ bbb-playback/data/var/bigbluebutton/playback/presentation/2.3/precache-manifest.c4a2d93c84069643f40dcfaf74ac7c37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619853614eb0748f5fb0ff26d9bed9aa",
+    "url": "/playback/presentation/2.3/index.html"
+  },
+  {
+    "revision": "a1a00170ebff5119cbb4",
+    "url": "/playback/presentation/2.3/static/css/main.62d161b0.chunk.css"
+  },
+  {
+    "revision": "35fba7709f72b11f5ebd",
+    "url": "/playback/presentation/2.3/static/js/2.e43bf653.chunk.js"
+  },
+  {
+    "revision": "69a6e6b7219006efbd406654f20c02ec",
+    "url": "/playback/presentation/2.3/static/js/2.e43bf653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a00170ebff5119cbb4",
+    "url": "/playback/presentation/2.3/static/js/main.cb3d77b8.chunk.js"
+  },
+  {
+    "revision": "e729c23475f84cc6c654",
+    "url": "/playback/presentation/2.3/static/js/runtime-main.091763c1.js"
+  },
+  {
+    "revision": "9381a7e996d0c900320a087cf90fc451",
+    "url": "/playback/presentation/2.3/static/media/icons.9381a7e9.woff"
+  },
+  {
+    "revision": "f6c597940842b1b2d8b3185de41d20e6",
+    "url": "/playback/presentation/2.3/static/media/logo.f6c59794.png"
+  }
+]);
