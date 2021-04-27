62f6510bf787b8e267d2a322d84c573a8b3d8cd0
chandi <git@chandi.it>
2021-04-27 21:04:36 +0200

    bbb-webhooks: 1:2.3.0-8

--- bbb-webhooks/data/usr/local/bigbluebutton/bbb-webhooks/opts-global.sh
+++ bbb-webhooks/data/usr/local/bigbluebutton/bbb-webhooks/opts-global.sh
@@ -1,6 +1,6 @@
 OPTS="--vendor BigBlueButon -m ffdixon@bigbluebutton.org --url http://bigbluebutton.org/"
 
-if [ ! -f /etc/redhat-release ]; then
+if [ -f /etc/lsb-release ]; then
   find /var/lib/gems/ -name deb.rb | grep '/fpm.*/lib/fpm/package/deb.rb' | xargs -I{} sed -i 's/if attributes\[:deb_use_file_permissions/if !attributes\[:deb_use_file_permissions/g' {}
 fi
  
